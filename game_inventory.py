848f11d53d323f5a33e681a67bad8a606901f607
Krzysiek Czaja <krzysiekczaja@gmail.com>
2019-01-04 23:39:06 +0100

    Some cleaning and pep8 adjustations.

--- game_inventory.py
+++ game_inventory.py
@@ -3,12 +3,13 @@
 # Write code in the functions (and create new functions) so that they work
 # according to the specification.
 
+
 def get_total_items_count(inventory):
     """Caounts total number of items in inventory and terurn it as int."""
     total_items = 0
     for i in inventory:
-      total_items += inventory[i]
-    
+        total_items += inventory[i]
+
     return total_items
 
 
@@ -16,27 +17,29 @@ def display_inventory(inventory):
     '''Display the inventory.'''
     print("Inventory:")
     for i in inventory:
-      print("{0} {1}".format(inventory[i], i))
-    
-    print("Total number of items: {0}". format(get_total_items_count(inventory)))    
+        print("{0} {1}".format(inventory[i], i))
+
+    print("Total number of items: {0}".
+          format(get_total_items_count(inventory)))
 
 
 def add_to_inventory(inventory, added_items):
     '''Add to the inventory dictionary a list of items from added_items.'''
     for i in added_items:
-      if i in inventory:
-        inventory[i] +=1
-      elif i not in inventory:
-        inventory[i] = 1
-    return inventory  
+        if i in inventory:
+                inventory[i] += 1
+        elif i not in inventory:
+                inventory[i] = 1
+    return inventory
+
 
 def get_max_key_length(inventory_keys):
     """Determinates longest key from inventory_keys and return in as int"""
     max_key_length = 0
     for i in inventory_keys:
-      if len(i) > max_key_length:
-        max_key_length = len(i)
-    
+        if len(i) > max_key_length:
+                max_key_length = len(i)
+
     return max_key_length
 
 
@@ -44,21 +47,23 @@ def get_max_value_length(inventory_values):
     """Determinates longest value from inventory_values and return in as int"""
     max_values_length = 0
     for i in inventory_values:
-      if len(str(i)) > max_values_length:
-        max_values_length = len(str(i))
-    
+        if len(str(i)) > max_values_length:
+                max_values_length = len(str(i))
+
     return max_values_length
 
 
 def get_sorted_inventory(inventory, order):
     """Sorts inventory by given order. Return ordered list of tuples."""
     if order == "count,asc":
-      inventory_sorted = sorted(list(inventory.items()), key=lambda item: item[1])
+        inventory_sorted = sorted(list(inventory.items()),
+                                  key=lambda item: item[1])
     elif order == "count,desc":
-      inventory_sorted = list(reversed(sorted(list(inventory.items()), key=lambda item: item[1])))
-    elif order == None:
-      inventory_sorted = list(inventory.items())
-      
+        inventory_sorted = list(reversed(sorted(list(inventory.items()),
+                                         key=lambda item: item[1])))
+    elif order is None:
+        inventory_sorted = list(inventory.items())
+
     return inventory_sorted
 
 
@@ -78,12 +83,17 @@ def print_table(inventory, order=None):
     sorted_inventory = get_sorted_inventory(inventory, order)
 
     print("Inventory:\n")
-    print("{0:>{width_c}}{1:>{width_i}}".format("Count", "item name", width_c = max_value_length + 4, width_i = max_key_length + 3))
+    print("{0:>{width_c}}{1:>{width_i}}".
+          format("Count", "item name", width_c=max_value_length + 4,
+                 width_i=max_key_length + 3))
     print("-" * (max_key_length + max_value_length + 7))
     for i in sorted_inventory:
-      print("{0:>{width_c}}{1:>{width_i}}".format(i[1], i[0], width_c = max_value_length + 4, width_i = max_key_length + 3))
+        print("{0:>{width_c}}{1:>{width_i}}".
+              format(i[1], i[0], width_c=max_value_length + 4,
+                     width_i=max_key_length + 3))
     print("-" * (max_key_length + max_value_length + 7))
-    print("Total number of items: {0}".format(get_total_items_count(inventory)))
+    print("Total number of items: {0}".
+          format(get_total_items_count(inventory)))
 
 
 def import_inventory(inventory, filename="import_inventory.csv"):
@@ -96,7 +106,7 @@ def import_inventory(inventory, filename="import_inventory.csv"):
     The file format is plain text with comma separated values (CSV).
     '''
     with open(filename, "r") as import_file:
-      imported_items = import_file.read().split(",")
+        imported_items = import_file.read().split(",")
 
     inventory = add_to_inventory(inventory, imported_items)
     print_table(inventory, "count,desc")
@@ -113,28 +123,9 @@ def export_inventory(inventory, filename="export_inventory.csv"):
     '''
     export_items = []
     for i in inventory:
-      for j in range(inventory[i]):
-        export_items.append(i)
-        
+        for j in range(inventory[i]):
+                export_items.append(i)
+
     export_items_string = ",".join(export_items)
     with open(filename, "w") as export_file:
-      export_file.write(export_items_string)
-
-
-#print("Step 1")
-inv = {'rope': 1, 'torch': 6, 'gold coin': 42, 'dagger': 1, 'arrow': 12}
-#display_inventory(inv)
-
-#print("\n\nStep 2")
-# dragon_loot = ['gold coin', 'dagger', 'gold coin', 'gold coin', 'ruby']
-# inv = add_to_inventory(inv, dragon_loot)
-#display_inventory(inv)
-
-#print("\n\nStep 3")
-#print_table(inv,"count,desc")
-
-#print("\n\nStep 4")
-#import_inventory(inv, "test_inventory.csv")
-
-#print("\n\nStep 5")
-export_inventory(inv)
+        export_file.write(export_items_string)
